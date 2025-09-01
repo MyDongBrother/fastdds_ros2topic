428909b5238497d9e9b73e3075a04f602e47b643
bmi-jiandong <liujiandong@bm-intelligent.com>
2025-09-01 18:19:32 +0800

    和apa的PC上联调

--- src/polygon_stamped_sub_ros/src/PolygonFramePubSubMain.cxx
+++ src/polygon_stamped_sub_ros/src/PolygonFramePubSubMain.cxx
@@ -9,18 +9,31 @@ class ROS2PolygonPublisher : public rclcpp::Node
 public:
     ROS2PolygonPublisher() : Node("ros2_polygon_publisher")
     {
-        goal_footprint_pub_ =
+        curpos_pub_ =
             this->create_publisher<geometry_msgs::msg::PolygonStamped>(
-                "/global_costmap/start_footprint", 10);
+                "/CurPos", 10);
+        slot_pub_ =
+            this->create_publisher<geometry_msgs::msg::PolygonStamped>(
+                "/SlotInfo", 10);
     }
 
-    void PublishFootprints(const geometry_msgs::msg::PolygonStamped &poly_msg)
+    void PublishFootprints(geometry_msgs::msg::PolygonStamped &poly_msg)
     {
-        goal_footprint_pub_->publish(poly_msg);
+        if (poly_msg.header.frame_id == "CurPos")
+        {
+            poly_msg.header.frame_id = "map";
+            curpos_pub_->publish(poly_msg);
+        }
+        else if (poly_msg.header.frame_id == "SlotInfo")
+        {
+            poly_msg.header.frame_id = "map";
+            slot_pub_->publish(poly_msg);
+        }
     }
 
 private:
-    rclcpp::Publisher<geometry_msgs::msg::PolygonStamped>::SharedPtr goal_footprint_pub_;
+    rclcpp::Publisher<geometry_msgs::msg::PolygonStamped>::SharedPtr curpos_pub_;
+    rclcpp::Publisher<geometry_msgs::msg::PolygonStamped>::SharedPtr slot_pub_;
 };
 
 int main(int argc, char **argv)
@@ -39,8 +52,9 @@ int main(int argc, char **argv)
     while (rclcpp::ok())
     {
         geometry_msgs::msg::PolygonStamped ros_msg;
-        mysub.run(ros_msg);
+        mysub.run(ros_msg); // 填充 frame_id ("CurPos" 或 "SlotInfo")
         node->PublishFootprints(ros_msg);
+
         rclcpp::spin_some(node);
         rate.sleep();
     }
